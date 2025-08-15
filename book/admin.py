9142317480168eceb49fda40be0c4c7a910d151a
shohjahonsherzodov <uzbuzb314@gmail.com>
2025-08-15 18:07:46 +0500

    upgrade

--- book/admin.py
+++ book/admin.py
@@ -2,7 +2,9 @@
 from django.contrib import admin
 from .models import Book, Author, Category, Comment, Rating, Feed
 
-admin.site.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    search_fields = ("title", )
+admin.site.register(Book, BookAdmin)
 admin.site.register(Author)
 admin.site.register(Category)
 admin.site.register(Comment)
